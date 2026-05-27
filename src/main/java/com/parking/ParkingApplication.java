93cc468c9a1fc0c9328d348011f4bc95008280bf
Jorge Garcia <geor.1442@gmail.com>
2026-05-26 22:34:47 -0500

    feat: add parameter catalog with caffeine cache

--- src/main/java/com/parking/ParkingApplication.java
+++ src/main/java/com/parking/ParkingApplication.java
@@ -2,10 +2,11 @@ package com.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication(scanBasePackages = "com.parking")
+@SpringBootApplication
+@EnableCaching     
 public class ParkingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ParkingApplication.class, args);
     }
